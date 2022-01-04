7894937bd8e1a027848297e16bf5af29e5530b02
JethroSama <kel73896@gmail.com>
2022-01-05 00:00:39 +0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <p className="text-red-800">Hello World</p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="flex items-center px-5 py-2 justify-between">
+            <h1 className="text-3xl font-bold mr-5">Url Shorten</h1>
+
+            <div>
+                Dark mode
+            </div>
+        </div>
+    )
+}
+
+export default Header
