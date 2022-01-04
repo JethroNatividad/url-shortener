fae343a1372936d8f7ac345ac0df404360475b0e
JethroSama <kel73896@gmail.com>
2022-01-05 02:12:02 +0800

    Make url open new page

--- src/components/UrlList.js
+++ src/components/UrlList.js
@@ -11,8 +11,8 @@ const UrlList = ({ urls }) => {
                 </tr>
                 {urls.map((url, index) => (
                     <tr key={index} className='bg-white shadow-sm shadow-gray-400'>
-                        <td className="px-3 py-1 text-blue-800 hover:underline"><a href={url.main_url}>{url.main_url}</a></td>
-                        <td className="px-3 py-1 text-blue-800 hover:underline"><a href={url.short_url}>{url.short_url}</a></td>
+                        <td className="px-3 py-1 text-blue-800 hover:underline"><a href={url.main_url} target="_blank">{url.main_url}</a></td>
+                        <td className="px-3 py-1 text-blue-800 hover:underline"><a href={url.short_url} target="_blank">{url.short_url}</a></td>
                         <td className="px-3 py-1">{url.clicks}</td>
                     </tr>
                 ))}
