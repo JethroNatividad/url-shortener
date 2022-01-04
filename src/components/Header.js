7d314e34158afd9395d260de8cf39d29b8e41e4e
JethroSama <kel73896@gmail.com>
2022-01-05 00:07:07 +0800

    add light mode icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { BsSun } from 'react-icons/bs'
 const Header = () => {
     return (
         <div className="flex items-center px-5 py-2 justify-between">
             <h1 className="text-3xl font-bold mr-5">Url Shorten</h1>
 
-            <div>
-                Dark mode
+            <div className="rounded-3xl border-2 border-black p-1">
+                <BsSun className="text-2xl" />
             </div>
         </div>
     )
