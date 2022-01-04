badf9182204ef9b2342a986fca4a7c96b15ee12a
JethroSama <kel73896@gmail.com>
2022-01-05 00:34:38 +0800

    Add content input and style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Content from './components/Content';
 import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen bg-gray-100">
       <Header />
       <Content />
     </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { AiOutlineLink } from 'react-icons/ai'
 const Content = () => {
     return (
-        <div>
-            Content
+        <div className="flex flex-col items-center mt-48">
+            <div>
+                <h1 className="font-bold text-5xl text-violet-900">Create Short Links!</h1>
+                <p className="text-center mt-5">free URL shortener tool</p>
+            </div>
+            <div className="mt-10 w-1/3 h-40 rounded-2xl px-5 py-10 shadow-gray-400 shadow-lg bg-white">
+                <div className="bg-gray-100 flex p-3 rounded-xl items-center">
+                    <AiOutlineLink className="text-2xl mr-2 text-gray-400" />
+                    <input className="outline-none bg-transparent flex-1 text-xl" placeholder="Paste link to shorten it" />
+                    <button type="button" className="w-20 bg-gradient-to-l from-violet-600 to-violet-400 rounded-lg px-3 py-2 text-gray-100 shadow-gray-400 shadow-lg">Shorten</button>
+                </div>
+            </div>
         </div>
     )
 }
