cf6b51658540f8d99589270b97c466d98b3a540c
JethroSama <kel73896@gmail.com>
2022-01-05 01:19:43 +0800

    create add url function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Content from './components/Content';
 import Header from './components/Header';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from './firebase'
 import UrlList from './components/UrlList';
 
@@ -19,6 +19,20 @@ function App() {
     }
     return main();
   }, [])
+
+  async function AddNewUrl(url) {
+    try {
+      const docRef = await addDoc(collection(db, "urls"), {
+        main_url: "Ada",
+        short_url: "Lovelace",
+        clicks: 0
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   return (
     <div className="App h-screen bg-gray-100">
       <Header />
