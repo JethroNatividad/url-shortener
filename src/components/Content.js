9bbcd0a4d023419255e2c10785c66aaddc794a28
JethroSama <kel73896@gmail.com>
2022-01-05 01:24:17 +0800

    set controlled input state

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AiOutlineLink } from 'react-icons/ai'
 const Content = ({ shortenUrl }) => {
-
+    const [input, setInput] = React.useState("");
     return (
         <div className="flex flex-col items-center mt-48">
             <div>
@@ -11,8 +11,11 @@ const Content = ({ shortenUrl }) => {
             <div className="mt-10 w-1/3 h-40 rounded-2xl px-5 py-10 shadow-gray-400 shadow-lg bg-white">
                 <div className="bg-gray-100 flex p-3 rounded-xl items-center">
                     <AiOutlineLink className="text-2xl mr-2 text-gray-400" />
-                    <input className="outline-none bg-transparent flex-1 text-xl" placeholder="Paste link to shorten it" />
-                    <button type="button" className="w-20 bg-gradient-to-l from-violet-600 to-violet-400 rounded-lg px-3 py-2 text-gray-100 shadow-gray-400 shadow-lg">Shorten</button>
+                    <input value={input} onChange={(e) => { setInput(e.currentTarget.value) }} className="outline-none bg-transparent flex-1 text-xl" placeholder="Paste link to shorten it" />
+                    <button onClick={() => {
+                        shortenUrl(input)
+                        setInput("")
+                    }} type="button" className="w-20 bg-gradient-to-l from-violet-600 to-violet-400 rounded-lg px-3 py-2 text-gray-100 shadow-gray-400 shadow-lg">Shorten</button>
                 </div>
             </div>
         </div>
