00e87637a88b5b639780533b29a4d9b02a4674cf
JethroSama <kel73896@gmail.com>
2022-01-11 14:51:12 +0800

    select text urls

--- src/components/UrlList.js
+++ src/components/UrlList.js
@@ -10,7 +10,7 @@ const UrlList = ({ urls }) => {
                     <td className="px-3 py-1 text-gray-500 dark:text-offWhite-100">Visits</td>
                 </tr>
                 {urls.map((url, index) => (
-                    <tr key={index} className='bg-white dark:bg-gray-600 dark:text-offWhite-100 shadow-sm shadow-gray-400 dark:shadow-gray-600'>
+                    <tr key={index} className='bg-white dark:bg-gray-600 dark:text-offWhite-100 shadow-sm shadow-gray-400 dark:shadow-gray-600 select-text'>
                         <td className="px-3 py-1 text-blue-800 dark:text-blue-200 hover:underline"><a href={url.main_url} target="_blank" rel="noreferrer">{url.main_url}</a></td>
                         <td className="px-3 py-1 text-blue-800 dark:text-blue-200 hover:underline"><a href={url.short_url} target="_blank" rel="noreferrer">{url.short_url}</a></td>
                         <td className="px-3 py-1">{url.clicks}</td>
