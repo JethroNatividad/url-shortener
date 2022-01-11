a3e047b94dfa437ed5e21a7c6262390da88267b0
JethroSama <kel73896@gmail.com>
2022-01-11 14:49:35 +0800

    darkmode toggle cursor pointer

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="flex items-center px-5 py-2 justify-between">
             <h1 className="text-3xl font-bold mr-5 dark:text-gray-200">Url Shorten</h1>
 
-            <div className="rounded-3xl border-2 border-black p-1 dark:border-white dark:text-white" onClick={toggleDarkMode} >
+            <div className="rounded-3xl border-2 border-black p-1 dark:border-white dark:text-white cursor-pointer" onClick={toggleDarkMode} >
                 {darkMode ? <BsSun className="text-2xl" /> : <BsMoon className="text-2xl" />}
                 {/* <BsMoon className="text-2xl" /> */}
             </div>
